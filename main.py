69da4a5312d62bc829f969dd55bf1eddeae8f919
bhushandeodhar <deodharbm@gmail.com>
2023-05-07 22:13:40 -0400

    Test Commit-1

--- main.py
+++ main.py
@@ -1,16 +1,71 @@
-# This is a sample Python script.
+import json
+import os
+from typing import Literal, Optional
+from uuid import uuid4
+from fastapi import FastAPI, HTTPException
+import random
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from mangum import Mangum
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+class Book(BaseModel):
+    name: str
+    genre: Literal["fiction", "non-fiction"]
+    price: float
+    book_id: Optional[str] = uuid4().hex
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+BOOKS_FILE = "books.json"
+BOOKS = []
+
+if os.path.exists(BOOKS_FILE):
+    with open(BOOKS_FILE, "r") as f:
+        BOOKS = json.load(f)
+
+app = FastAPI()
+handler = Mangum(app)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm GitHub')
+@app.get("/")
+async def root():
+    return {"message": "Welcome to my bookstore app!"}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@app.get("/random-book")
+async def random_book():
+    return random.choice(BOOKS)
+
+
+@app.get("/list-books")
+async def list_books():
+    return {"books": BOOKS}
+
+
+@app.get("/book_by_index/{index}")
+async def book_by_index(index: int):
+    if index < len(BOOKS):
+        return BOOKS[index]
+    else:
+        raise HTTPException(404, f"Book index {index} out of range ({len(BOOKS)}).")
+
+
+@app.post("/add-book")
+async def add_book(book: Book):
+    book.book_id = uuid4().hex
+    json_book = jsonable_encoder(book)
+    BOOKS.append(json_book)
+
+    with open(BOOKS_FILE, "w") as f:
+        json.dump(BOOKS, f)
+
+    return {"book_id": book.book_id}
+
+
+@app.get("/get-book")
+async def get_book(book_id: str):
+    for book in BOOKS:
+        if book.book_id == book_id:
+            return book
+
+    raise HTTPException(404, f"Book ID {book_id} not found in database.")
